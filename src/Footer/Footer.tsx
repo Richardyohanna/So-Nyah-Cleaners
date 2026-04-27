e82a926d1dae43854ad662c15a006571d6441d38
Richard Yohanna <yohanrichard091@gmail.com>
2026-04-27 15:38:02 +0100

    updated the so-nyah newsletter section

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -20,9 +20,10 @@ const Footer = () => {
   return (
     <section
       id="footer"
-      className="bottom-0 bg-[var(--primary)] px-4 sm:px-6 md:px-10 lg:px-10 pt-16 sm:pt-20 lg:pt-30 pb-10"
+      className="bottom-0 bg-[var(--primary)] px-4 sm:px-6 md:px-10 lg:px-10 pt-16 sm:pt-8 lg:pt-10 pb-10"
     >
-      <div className="bg-[var(--primary)] flex flex-col md:flex-row md:flex-wrap lg:flex-nowrap justify-between gap-10 lg:gap-6">
+
+      <div className="bg-[var(--primary)] mt-10 flex flex-col md:flex-row md:flex-wrap lg:flex-nowrap justify-between gap-10 lg:gap-6">
         <div className="flex flex-col gap-6 sm:gap-8 lg:gap-10 max-w-[320px]">
           <img src={logo} alt="" className="w-[130px] sm:w-[150px] bg-white p-3 h-auto cursor-pointer" onClick={() => navigate("/")} />
           <p className="!text-[#fffff2] max-w-[300px] text-sm sm:text-base leading-7">
@@ -100,6 +101,20 @@ const Footer = () => {
           </p>
         </div>
       </div>
+
+      <div className="pt-5">
+         <div className="w-full lg:max-w-[500px]">
+                <div className="bg-white rounded-2xl p-3 sm:p-4 shadow-2xl">
+                  <div className="flex flex-col sm:flex-row gap-3">
+                    <input type="email" placeholder="Enter your email address"
+                      className="h-[52px] sm:h-[56px] px-4 sm:px-5 rounded-xl bg-[rgba(246,243,242,1)] w-full outline-none text-[var(--primary)] placeholder:text-[var(--primary)]/60 text-sm sm:text-base" />
+                    <button className="h-[52px] sm:h-[56px] px-6 sm:px-7 rounded-xl bg-[var(--primary)] text-white font-medium transition-all duration-300 hover:opacity-90 hover:scale-[1.02] whitespace-nowrap">Subscribe</button>
+                  </div>
+                  <p className="text-[12px]! sm:text-sm! text-[#666]! mt-3 px-1 leading-6">By subscribing, you agree to receive email updates from So-nyah Cleaners.</p>
+                </div>
+           </div>
+      </div>
+
 
       <div className="flex flex-col md:flex-row w-full justify-between items-start md:items-center border-t-2 border-white pt-4 gap-4 md:gap-5 mt-12 sm:mt-16 lg:mt-20">
         <p className="flex-1 !text-white text-sm sm:text-base">
